ad1650858a2ee20ae99201e34c3f0fcf522f9740
Nurillo <nurillod23@mail.ru>
2023-09-14 13:49:14 +0500

    Initial Commit

---
+++ Self Sightseeing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Self Sightseeing
+//
+//  Created by Nurillo Domlajonov on 14/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
